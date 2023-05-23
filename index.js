b5d90be585806fb46808da5a63712dfaa40342fe
Roscoe <emgaralla@mail.ru>
2023-05-23 17:48:42 +0300

    bye backend

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.listen(port, () => {
+    console.log('sss')
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.get('/users', (req, res) => {
+    res.send(["user1", "user2", "user3"])
+})
+
+app.post('/users', (req, res) => {
+    res.send('юзер добавлен')
+})
+
+app.delete('/users/:id', (req, res) => {
+    res.send('юзер с ID :id удален')
+})
+
+app.patch('/users/:id', (req, res) => {
+    res.send('юзер с ID :id изменен')
+})
+
+app.get('/admin', (req, res) => {
+    res.send('не хватает прав доступа')
+})
